f5aa4ad365e4d157b08eaf3559b105ea0032910e
Travis Chase <travis.chase@gmail.com>
2018-07-10 20:12:54 -0600

    changed resolution to fit more of an atari-like display
    added gif of original map Warren Robinett had on his site about adventure

--- src/app.js
+++ src/app.js
@@ -9,10 +9,16 @@ import LoadingScene from './scenes/loading';
 import MainMenuScene from './scenes/main-menu';
 import PlayGameScene from './scenes/play-game';
 
+const EMULATED_ATARI_DISPLAY_WIDTH = 320;
+const EMULATED_ATARI_DISPLAY_HEIGHT = 200;
+const DISPLAY_MULTIPLIER = 3;
+const GAME_WIDTH = EMULATED_ATARI_DISPLAY_WIDTH * DISPLAY_MULTIPLIER;
+const GAME_HEIGHT = EMULATED_ATARI_DISPLAY_HEIGHT * DISPLAY_MULTIPLIER;
+
 var gameConfig = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
     backgroundColor: '#000000',
     physics: {
         default: 'arcade',
