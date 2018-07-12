a0370f1def661a0dad25be6eb8718dd6eb30e9cc
Travis Chase <travis.chase@gmail.com>
2018-07-11 21:39:48 -0600

    added some screenshots
    added general game config json file
    
    changed game dimensions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'phaser';
 import 'lodash';
 import 'webfontloader';
+import config from './config/game.json';
 
 require('./index.html'); // so we get it in the dist
 
@@ -9,16 +10,10 @@ import LoadingScene from './scenes/loading';
 import MainMenuScene from './scenes/main-menu';
 import PlayGameScene from './scenes/play-game';
 
-const EMULATED_ATARI_DISPLAY_WIDTH = 320;
-const EMULATED_ATARI_DISPLAY_HEIGHT = 200;
-const DISPLAY_MULTIPLIER = 3;
-const GAME_WIDTH = EMULATED_ATARI_DISPLAY_WIDTH * DISPLAY_MULTIPLIER;
-const GAME_HEIGHT = EMULATED_ATARI_DISPLAY_HEIGHT * DISPLAY_MULTIPLIER;
-
 var gameConfig = {
     type: Phaser.AUTO,
-    width: GAME_WIDTH,
-    height: GAME_HEIGHT,
+    width: config.gameWidth,
+    height: config.gameHeight,
     backgroundColor: '#000000',
     physics: {
         default: 'arcade',
